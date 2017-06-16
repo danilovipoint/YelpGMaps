91e331a8b17811400bac912cbc47ebb243f7c3ef
Alexey Danilov <alexeydanilov@MacBook-Pro-Alexey.local>
2017-06-16 16:16:08 +0800

    update icons and about text

--- YelpGMaps/Controllers/AboutViewController.swift
+++ YelpGMaps/Controllers/AboutViewController.swift
@@ -21,7 +21,7 @@ class AboutViewController: ParentViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let text = "This applications is developed for demonstration purposes. Application is developed in iPoint LLC. Our site is www.ipoint-consulting.com. Displayed data is getting from https://www.yelp.com and Yelp Fusion API is used for requesting data. Google Maps are used for displaying data on the map. iPoint Logo image is property of iPoint LLC. All other icons and images are free licensed and getting from https://www.iconfinder.com and https://design.google.com/icons/ With offers and questions please write to us on emails danilov@ipoint.ru and dev@ipoint.ru. Thanks for your attention."
+        let text = "This applications is developed in iPoint LLC. Our site is www.ipoint-consulting.com. Displayed data is getting from https://www.yelp.com and Yelp Fusion API is used for requesting data. Google Maps are used for displaying data on the map. iPoint Logo image is property of iPoint LLC. All other icons and images are free licensed and getting from https://www.iconfinder.com and https://design.google.com/icons/ With offers and questions please write to us on emails danilov@ipoint.ru and dev@ipoint.ru. Thanks for your attention."
         
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 8.0
